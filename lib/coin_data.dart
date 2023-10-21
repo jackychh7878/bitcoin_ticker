18024e0d634764729de3e18d15316b7a34563c08
jacky-Window <jackychh7878@gmail.com>
2023-10-21 19:17:36 +0800

    done

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+import 'dart:collection';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,52 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'YOUR-API-KEY-HERE';
+
+const newCoinAPIURL = 'https://api.coingate.com/v2/rates/merchant';
+
+class NetworkHelper{
+
+  NetworkHelper({required this.url});
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200){
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
+
+class CoinData {
+  //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+  //TODO 5: Return a Map of the results instead of a single value.
+  Future<dynamic> getCoinData(String crypto, String currency) async {
+    var url = '$newCoinAPIURL/$crypto/$currency';
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    double exchangeRate = await networkHelper.getData();
+
+    return exchangeRate;
+  }
+
+  Future<HashMap<String, HashMap<String, double>>> getCoinListData(List<String> cryptoList, List<String> currenciesList) async {
+    HashMap<String, HashMap<String, double>> cryptoCurrencyMap = HashMap();
+    for(String crypto in cryptoList) {
+      HashMap<String, double> exchangeRateMap = HashMap();
+      for(String currency in currenciesList) {
+        var url = '$newCoinAPIURL/$crypto/$currency';
+        NetworkHelper networkHelper = NetworkHelper(url: url);
+        double exchangeRate = await networkHelper.getData();
+        exchangeRateMap.addAll({currency: exchangeRate});
+      }
+      cryptoCurrencyMap.addAll({crypto: exchangeRateMap});
+    }
+    return cryptoCurrencyMap;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -8,6 +12,99 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
 
   String selectedCurrency = 'USD';
+  String cryptoCurrency = 'BTC';
+  double exchangeRateData = -1;
+  var exchangeRateDataList;
+  List<CryptoCard> cryptoCardList = [];
+
+
+  DropdownButton<String> androidDropdown () {
+    List<DropdownMenuItem<String>> dropdownItemsList = [];
+    for (String currency in currenciesList){
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItemsList.add(newItem);
+    }
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownItemsList,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value!;
+          cryptoCardList = [];
+          setCryptoCardList();
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iosPicker() {
+    List<Text> pickerItemsList = [];
+    for(String currency in currenciesList){
+      pickerItemsList.add(Text(currency));
+    }
+    return CupertinoPicker(
+      itemExtent: 32,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+          cryptoCardList = [];
+          setCryptoCardList();
+        });
+      },
+      children: pickerItemsList,
+    );
+  }
+
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      return iosPicker();
+    } else if (Platform.isAndroid) {
+      return androidDropdown();
+    }
+    return Container();
+  }
+
+  //TODO 7: Figure out a way of displaying a '?' on screen while we're waiting for the price data to come back. Hint: You'll need a ternary operator.
+
+  //TODO 6: Update this method to receive a Map containing the crypto:price key value pairs. Then use that map to update the CryptoCards.
+
+  void getData() async {
+    exchangeRateData = await CoinData().getCoinData(cryptoCurrency, selectedCurrency);
+  }
+
+  void getDataList() async {
+    setCryptoCardList();
+    var fetchingExchangeRateDataList = await CoinData().getCoinListData(cryptoList, currenciesList);
+    print(fetchingExchangeRateDataList);
+    setState(() {
+      exchangeRateDataList = fetchingExchangeRateDataList;
+    });
+  }
+
+  void setCryptoCardList() {
+    if (exchangeRateDataList != null) {
+      for(String crypto in cryptoList) {
+        cryptoCardList.add(CryptoCard(exchangeRateData: exchangeRateDataList[crypto][selectedCurrency], selectedCrypto: crypto, selectedCurrency: selectedCurrency));
+      }
+    } else {
+      for(String crypto in cryptoList) {
+        cryptoCardList.add(CryptoCard(exchangeRateData: -1, selectedCrypto: crypto, selectedCurrency: selectedCurrency));
+      }
+    }
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    getDataList();
+  }
+  //TODO: For bonus points, create a method that loops through the cryptoList and generates a CryptoCard for each.
+
 
   @override
   Widget build(BuildContext context) {
@@ -19,56 +116,65 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
+          //TODO 1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+          //TODO 2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
+          //TODO 3: You'll need to use a Column Widget to contain the three CryptoCards.
+          exchangeRateDataList == null ?
+              Center(
+                child: SpinKitDoubleBounce(
+                  color: Colors.black,
+                  size: 100,
                 ),
-              ),
-            ),
+              )
+              : Column(
+            children: cryptoCardList,
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: selectedCurrency,
-              items: [
-                DropdownMenuItem(
-                  child: Text('USD'),
-                  value: 'USD',
-                ),
-                DropdownMenuItem(
-                  child: Text('EUR'),
-                  value: 'EUR',
-                ),
-                DropdownMenuItem(
-                  child: Text('GBP'),
-                  value: 'GBP',
-                ),
-              ],
-              onChanged: (value) {
-                setState(() {
-                  selectedCurrency = value!;
-                });
-              },
-            ),
+            child: Platform.isAndroid ? iosPicker() : androidDropdown(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CryptoCard extends StatelessWidget {
+  const CryptoCard({
+    super.key,
+    required this.exchangeRateData,
+    required this.selectedCrypto,
+    required this.selectedCurrency,
+  });
+
+  final double exchangeRateData;
+  final String selectedCrypto;
+  final String selectedCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            exchangeRateData != -1 ? '1 $selectedCrypto = $exchangeRateData $selectedCurrency' : '1 $selectedCrypto = ? $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
